0821ec40625d11318adc2a75759db4a1945554fe
kriangkrai <st651102064101@pcru.ac.th>
2026-03-28 02:41:01 +0700

    Add full-width styling to Google Sign-In button in LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -76,7 +76,7 @@
 
                     <!-- Google Sign-In Button -->
                     <div class="google-btn-container mb-4">
-                        <button type="button" class="btn btn-google rounded-3" @click="handleGoogleLogin" :disabled="isGoogleLoading">
+                        <button type="button" class="btn btn-google rounded-3 w-100" @click="handleGoogleLogin" :disabled="isGoogleLoading">
                             <span v-if="isGoogleLoading" class="spinner-border spinner-border-sm me-2" aria-hidden="true"></span>
                             <svg v-else class="google-icon me-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="24" height="24">
                               <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
